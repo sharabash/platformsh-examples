09fb86dfa4924d1c94ce2cfd91c2033ec77dee64
GuGuss <augustin@commerceguys.com>
2015-02-03 18:13:36 +0100

    Prestashop can't have an empty database user and needs a version.

--- config-init/settings_platform.inc.php
+++ config-init/settings_platform.inc.php
@@ -14,8 +14,11 @@ foreach ($relationships['database'] as $endpoint) {
   /* Set default database credentials using platform environment variables. */
   define('_DB_SERVER_', $endpoint['host']);
   define('_DB_NAME_', $endpoint['path']);
-  define('_DB_USER_', $endpoint['username']);
+  define('_DB_USER_', 'web');
   define('_DB_PASSWD_', $endpoint['password']);
   define('_DB_PREFIX_', 'ps_');
   define('_MYSQL_ENGINE_', 'InnoDB');
+
+  if (!defined('_PS_VERSION_'))
+  define('_PS_VERSION_', '1.6.0.11');
 }
