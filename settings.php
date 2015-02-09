c0d5074895069cfde7bc7e59e0bd845ed3b2f1bc
Augustin Delaporte <augustin@commerceguys.com>
2015-02-09 11:28:52 +0100

    Adjust the order of container yamls to override settings per environment
    See the related Drupal issue here: https://www.drupal.org/node/2381763

--- settings.php
+++ settings.php
@@ -8,6 +8,12 @@ $config_directories = array(
 # Modify this to make it specific to your project.
 $settings['hash_salt'] = 'zaAPBHIVJmNIRywCSMegjmt9ST3IpUFVWef3SQKl';
 
+# Adjust the order of container yamls to override settings per environment.
+if(!$application_home = getenv('PLATFORM_APP_DIR')) {
+  $application_home = '/app';
+}
+$settings['container_yamls'][] = $application_home . '/services.yml';
+
 $local_settings = dirname(__FILE__) . '/settings.local.php';
 if (file_exists($local_settings)) {
   require_once($local_settings);
