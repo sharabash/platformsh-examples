90f883e0225a36c36150af5a04900be151d14352
Patrick Dawkins <pjcdawkins@gmail.com>
2015-03-29 00:12:52 +0000

    Specify install profile in settings.php

--- settings.php
+++ settings.php
@@ -4,6 +4,9 @@
  * Platform.sh example settings.php file for Drupal 8.
  */
 
+// Install with the 'standard' profile for this example.
+$settings['install_profile'] = 'standard';
+
 // You should modify the hash_salt so that it is specific to your application.
 $settings['hash_salt'] = '4946c1912834b8477cc70af309a2c30dcec24c2103c724ff30bf13b4c10efd82';
 
